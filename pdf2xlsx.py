0b06020f73a3ab5f915fe1d9a8d13d00869bb87a
BobDev94 <108867512+BobDev94@users.noreply.github.com>
2024-01-09 17:31:36 +0545

    Update pdf2xlsx.py

--- pdf2xlsx.py
+++ pdf2xlsx.py
@@ -6,14 +6,16 @@ def main():
     pdf_path = input("""\nNew Excel file will be created in the same directory as the pdf file. 
 \nPlease enter pdf file path: """)
     
+    # pdf_path = "C:\Python\Python310\Programs\Calculus_Demystified.pdf"
+
     match = re.search(r"(.*)\\(\w+)\.pdf$",pdf_path)
     excel_path = match.group(1) + '\\' + match.group(2) + '.xlsx'
     print(f'\nExcel path: {excel_path}')
 
-    opt = input("\nHit enter to convert all tables in the pdf file into excel sheets, enter number to convert specific page: ")
+    opt = input("\nHit enter to convert all tables in the pdf file into excel sheets, enter number to convert specific page, and for multiple pages, enter the numbers separated by commas example: '1-5' or '1,3,5': ")
     if not opt:
         opt = 'all'
-    else:
+    elif opt.isdigit():
         opt = int(opt)
 
     dfs =  tabula.read_pdf(pdf_path, stream = True, pages = opt)
